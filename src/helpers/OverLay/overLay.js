e3b28218b7ca1d90f340c40fe25f5873cbf01b25
Chris Phan <nhat.phan@uconn.edu>
2018-09-17 16:11:49 -0400

    Change from Layover Hover to onCLick

--- src/helpers/OverLay/overLay.js
+++ src/helpers/OverLay/overLay.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './overlay.css';
 import FontAwesome from 'react-fontawesome';
-const LayOver = (props) => {
-    return (
-        <div className={props.classes}>
-            <div className="layover-container">
-                <div className="layover-first-layer">
-                    <h1>{props.topTitle}</h1>
-                </div>
-                <div className="layover-second-layer">
-                    <ul>
-                    
-                        {props.bottomList.map(eachElement => <li>{eachElement}</li>)}
-                    </ul>
-                    <div class="arrow-animation">
-                    <a href="#">&#8595;</a>
-                    </div>
-                </div>
-              
-            </div>
-        </div>
+class LayOver extends Component {
+    state = {
+        active: false
+    }
+    toggleState() {
+        this.setState(prevState => ({
+            active: !prevState.active
+        })
+        )
+    }
+    
 
-    )
+    render() {
+        const firstLayerClasses=["layover-first-layer"]
+        const secondLayerClasses=["layover-second-layer"]
+        if(this.state.active){
+            firstLayerClasses.push("layover-first-layer-clicked")
+            secondLayerClasses.push("layover-second-layer-clicked")
+        }
+        return (
+            <div className={this.props.classes}>
+                <div className="layover-container">
+                    <div className={firstLayerClasses.join(" ")} onClick={this.toggleState.bind(this)}>
+                        <h1>{this.props.topTitle}</h1>
+                    </div>
+                    <div className={secondLayerClasses.join(" ")}>
+                        <ul>
+
+                            {this.props.bottomList.map(eachElement => <li>{eachElement}</li>)}
+                        </ul>
+                        <div class="arrow-animation">
+                            <a href="#">&#8595;</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        )
+    }
 }
 export default LayOver;
